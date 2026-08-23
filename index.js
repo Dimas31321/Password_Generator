a74588448f8d230dfad96003c407b409e541e046
Dmitry <dl030507@gmail.com>
2026-08-23 16:55:29 +0300

    Changed the button location

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ const characters = [
 let firstOption = document.getElementById("first-password-el")
 let secondOption = document.getElementById("second-password-el")
 let thirdOption = document.getElementById("third-password-el")
+let fourthOption = document.getElementById("fourth-password-el")
 function _generatePassword(){ //returns generated password with a length of 16
     let password = ""
     for (let i = 0; i<16; i+=1){
@@ -39,4 +40,5 @@ function generateOptions(amount=3){
     firstOption.textContent = options[0]
     secondOption.textContent = options[1]
     thirdOption.textContent = options[2]
+    fourthOption.textContent = options[3]
 }
